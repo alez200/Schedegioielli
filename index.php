0727d61eed8654af0cece99ac16afa2cf096cbf1
alez200 <alez200@yahoo.it>
2016-10-17 16:52:24 +0200

    Primo Caricamento

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bakeca
+ * Date: 17/10/2016
+ * Time: 16:41
+ */
+
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Schede gioielli</title>
+    </head>
+
+    <body>
+
+        <!-- COLLANE -->
+        <section>
+            <h1>COLLANE</h1>
+
+            <article>
+
+                <h2>Titolo prodotto</h2>
+                <img alt="foto" title="foto" src="" />
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+            </article>
+        </section>
+
+        <!-- ANELLI -->
+        <section>
+            <h1>ANELLI</h1>
+        </section>
+
+        <!-- BRACCIALETTI -->
+        <section>
+            <h1>BRACCIALETTI</h1>
+        </section>
+
+        <!-- ORECCHINI -->
+        <section>
+            <h1>ORECCHINI</h1>
+        </section>
+
+
+    </body>
+
+</html>
